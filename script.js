030552539038445bbc982fed6f55296de1da8cff
Gaël <gael77@hotmail.fr>
2022-01-23 16:30:33 +0100

    Séparation script + nouveau style

---
+++ script.js
@@ -0,0 +1,18 @@
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; // Non concerné si "transform" n'apparaît pas
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return; // Si aucun fichier audio ne correspond, arrête la fonction
+
+  key.classList.add("playing"); // Ajoute .playing à la div correspondant à la touche jouée
+  audio.currentTime = 0; // Redémarre l'audio au début
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
