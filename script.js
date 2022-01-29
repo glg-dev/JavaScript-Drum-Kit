d5fdddbdf018689447afd713d64a722ad2ee737f
Gaël <gael77@hotmail.fr>
2022-01-29 16:15:27 +0100

    Simplification code + debug clickevent

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ function removeTransition(e) {
   e.target.classList.remove("playing");
 }
 
+function letsPlay(touch, music) {
+  touch.classList.add("playing") // Ajoute .playing à la div correspondant à la touche jouée
+  music.currentTime = 0; // Redémarre l'audio au début
+  music.play()
+}
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
   const key = document.querySelector(`div[data-key="${e.key}"]`);
   if (!audio) return; // Si aucun fichier audio ne correspond, arrête la fonction
 
-  key.classList.add("playing"); // Ajoute .playing à la div correspondant à la touche jouée
-  audio.currentTime = 0; // Redémarre l'audio au début
-  audio.play();
+  letsPlay(key, audio)
 }
 
 const keys = Array.from(document.querySelectorAll(".key"));
@@ -19,10 +23,11 @@ keys.forEach(item => {
   item.addEventListener("click", () => {
     sounds.forEach(sound => {
       if (sound.dataset.key === item.dataset.key) {
-        sound.play()
+        letsPlay(item, sound)
       }
     });
   })
+  item.addEventListener("transitionend", removeTransition)
 });
 
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
